23978c7093e462e5a6941ebff4e13120451ef1cb
Eduard Ballweg <eduard.ballweg@gmail.com>
2025-11-07 20:38:01 +0100

    Web hook

--- server.js
+++ server.js
@@ -52,3 +52,7 @@ app.post("/submit", async (req, res) => {
 });
 
 app.listen(3000, () => console.log("✅ Server läuft auf http://localhost:3000"));
+app.post("/webhook", (req, res) => {
+  console.log("Webhook empfangen:", req.body);
+  res.sendStatus(200);
+});
